340945f83ba36950fab80b66c9d3887b0ff0a434
mayadebi <503991583@qq.com>
2021-09-14 00:10:06 +0800

    搭建eureka注册中心

---
+++ eureka-server/src/main/java/com/fzy/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.fzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// 开启Eureka的注解
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
