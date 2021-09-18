938da15e4c6578fbc93b721fbf0615a080c9f0d3
mayadebi <503991583@qq.com>
2021-09-18 14:43:54 +0800

    使用http客户端fegin代替RestTemplate 用接口形式代替http请求

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -6,9 +6,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+// 开启http客户端自动装配
+@EnableFeignClients
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
@@ -18,12 +20,12 @@ public class OrderApplication {
     }
 
     // 创建RestTemplate 注册到bean  这个是用来发http请求
-    @Bean
-    // 设置负载均衡
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
+//    @Bean
+//    // 设置负载均衡
+//    @LoadBalanced
+//    public RestTemplate restTemplate(){
+//        return new RestTemplate();
+//    }
 
     //负载均很策略  默认轮询
     // 这里是全局配置所有调用的服务都会生效
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -12,16 +13,20 @@ public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
-    // 注入RestTemplate
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
+    // 注入RestTemplate
+//    @Autowired
+//    private RestTemplate restTemplate;
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // 设置url
-        String url = "http://userservice/user/"+order.getUserId();
-        // 发起http请求
-        User user = restTemplate.getForObject(url, User.class);
+        // 使用feign调用
+        User user = userClient.findById(order.getUserId());
+//        // 设置url
+//        String url = "http://userservice/user/"+order.getUserId();
+//        // 发起http请求
+//        User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
         // 4.返回
         return order;
