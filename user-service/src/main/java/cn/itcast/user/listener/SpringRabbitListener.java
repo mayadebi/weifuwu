e56f7bb49bcb65538fe321a2d9d01883e49ccdfa
mayadebi <503991583@qq.com>
2021-09-28 19:50:11 +0800

    springAMQP配置多个消费者和每次只取一个

--- user-service/src/main/java/cn/itcast/user/listener/SpringRabbitListener.java
+++ user-service/src/main/java/cn/itcast/user/listener/SpringRabbitListener.java
@@ -5,8 +5,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringRabbitListener {
+//    @RabbitListener(queues = "simple.queue")
+//    public void ListEnSimpleQueue(String msg){
+//        System.out.println("接收到消息"+msg);
+//    }
     @RabbitListener(queues = "simple.queue")
-    public void ListEnSimpleQueue(String msg){
-        System.out.println("接收到消息"+msg);
+    public void ListEnSimpleQueue(String msg) throws InterruptedException {
+        System.out.println("消费者1接收到消息"+msg);
+        Thread.sleep(20);
+    }
+    @RabbitListener(queues = "simple.queue")
+    public void ListEnSimpleQueue2(String msg) throws InterruptedException {
+        System.out.println("消费者22222222222接收到消息"+msg);
+        Thread.sleep(200);
     }
 }
